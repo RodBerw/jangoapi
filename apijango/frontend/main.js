d4cca3c7e3da099c81ad8538fbdcd8c7c7047847
Rodolfo Berwanger Liberado <83656054+RodBerw@users.noreply.github.com>
2022-11-14 21:33:10 -0300

    Commit validações main.js

--- apijango/frontend/main.js
+++ apijango/frontend/main.js
@@ -1,25 +1,27 @@
 var url = "http://localhost:3000/";
 
+//BOOK
+
 function registerBook() {
   if (!validateCode("code")) {
     return;
   }
-  if (!validateName("name")) {
+  if (!validateString("name")) {
     return;
   }
 
-  if (!validaData("author")) {
+  if (!validateString("author")) {
     return;
   }
 
-  if (!validaSenha("genre")) {
+  if (!validateString("genre")) {
     return;
   }
 
-  if (!confirmaSenha("publisher")) {
+  if (!validateString("publisher")) {
     return;
   }
-  if (!confirmaSenha("year")) {
+  if (!validateYear("year")) {
     return;
   }
 
@@ -31,7 +33,68 @@ function registerBook() {
     Publisher: document.getElementById("publisher").value,
     Year: document.getElementById("year").value,
   };
+
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        console.log(request.responseText);
+        alert("Registro efetuado com sucesso!");
+      } else {
+        console.error(request.responseText);
+        alert("Não foi possível efetuar o registro!");
+      }
+    }
+  };
+  request.open("POST", url + "books");
+  request.setRequestHeader("Accept", "application/json");
+  request.setRequestHeader("Content-type", "application/json");
+  request.send(JSON.stringify(body));
 }
+
+///Book functions
+
+function validateCode(id) {
+  let input = document.getElementById(id);
+  let isNumber = /[0-9]/.test(input.value);
+  if (isNumber) {
+    input.style.border = 0;
+    return true;
+  } else {
+    input.style.border = "solid 1px red";
+    return false;
+  }
+}
+
+function validateString(id) {
+  let input = document.getElementById(id);
+  let hasString = /[a-z]/.test(input.value);
+  let hasStringM = /[A-Z]/.test(input.value);
+  let isNumber = /[0-9]/.test(input.value);
+
+  if (hasString || hasStringM || isNumber) {
+    input.style.border = 0;
+    return true;
+  } else {
+    input.style.border = "solid 1px red";
+    return false;
+  }
+}
+
+function validateYear(id) {
+  let input = document.getElementById(id);
+  let isNumber = /[0-9]/.test(input.value);
+
+  if (isNumber) {
+    input.style.border = 0;
+    return true;
+  } else {
+    input.style.border = "solid 1px red";
+    return false;
+  }
+}
+
+//CLIENT
 
 function registerClient() {
   if (!validateCode("code")) {
@@ -61,6 +124,8 @@ function registerClient() {
     Cpf: document.getElementById("cpf").value,
   };
 }
+
+//RENT
 
 function registerRent() {
   if (!validateCode("code")) {
